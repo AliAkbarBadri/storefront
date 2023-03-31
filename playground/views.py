695ba29c2b8552841f61d3017a2cb9cb83199eec
AliAkbarBadri <aabadri74@gmail.com>
2023-03-31 21:58:57 +0330

    add add some orm filters

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from store.models import Product
 from django.http import HttpResponse
 
+
 def say_hello(request):
-    return HttpResponse("hello world")
+    # product = Product.objects.get(pk=1)
+    # product = Product.objects.filter(pk=1).first()
+    # print(product.title)
+
+    # keyword = value
+    # query_set = Product.objects.filter(unit_price=20)
+    # query_set = Product.objects.filter(unit_price__gt=20)
+    # query_set = Product.objects.filter(unit_price__range=(20, 30))
+    # query_set = Product.objects.filter(collection__id__range=(1,3))
+    # query_set = Product.objects.filter(title__icontains='coffee')
+    query_set = Product.objects.filter(last_update__year=2021)
+    # query_set = Product.objects.filter(description__isnull=True)
+    return render(request, "hello.html", {"name": "Ali", "products": list(query_set)})
