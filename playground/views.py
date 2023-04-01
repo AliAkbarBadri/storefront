ee85bc116a69a8406fbad2042126af32564e66c0
AliAkbarBadri <aabadri74@gmail.com>
2023-04-01 17:27:57 +0330

    add group example

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.db.models import Value, F
+from django.db.models import Value, F, Func, Count
 from store.models import Product, Customer, OrderItem, Order
 
 
 def say_hello(request):
-    query_set = Customer.objects.annotate(is_new=Value(True))
-    query_set = Customer.objects.annotate(new_id=F("id") + 1)
+    # customer's number of orders
+    query_set = Customer.objects.annotate(
+        order_number = Count('order')
+    )
     _ = list(query_set)
     return render(request, "hello.html", {"name": "Ali"})
