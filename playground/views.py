4a4e2f9ff997b235df31f184b5d859574c8c4109
AliAkbarBadri <aabadri74@gmail.com>
2023-04-01 05:26:22 +0330

    add order

--- playground/views.py
+++ playground/views.py
@@ -5,5 +5,10 @@ from django.http import HttpResponse
 
 
 def say_hello(request):
-    query_set = []
+    # unit_prce ASC order, title DESC
+    query_set = Product.objects.order_by('unit_price', '-title')
+    
+    # first, last
+    product = Product.objects.earliest('unit_price')
+    product = Product.objects.last('unit_price')
     return render(request, "hello.html", {"name": "Ali", "products": list(query_set)})
