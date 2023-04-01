f403264612402e475c429de262903a6290df323b
AliAkbarBadri <aabadri74@gmail.com>
2023-04-01 05:58:25 +0330

    select fields to query

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product, Customer
+from store.models import Product, Customer, OrderItem
 from django.http import HttpResponse
 
 
 def say_hello(request):
-    # limit
-    query_set = Product.objects.all()[5:10]
+    # values
+    # dictionary
+    # query_set = Product.objects.values('id', 'title', 'collection__title')
+
+    # tuple
+    # query_set = Product.objects.values_list('id', 'title', 'collection__title')
+
+    # select products that have been ordered and sort them by title
+    query_set = (
+        OrderItem.objects.values(title=F("product__title")).distinct().order_by("title")
+    )
     return render(request, "hello.html", {"name": "Ali", "products": list(query_set)})
