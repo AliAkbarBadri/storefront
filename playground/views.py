7c3de955036c63006159afd6ac9f2ed4710a7321
AliAkbarBadri <aabadri74@gmail.com>
2023-04-01 15:30:55 +0330

    add aggregation examples

--- playground/views.py
+++ playground/views.py
@@ -1,21 +1,13 @@
 from django.shortcuts import render
-from django.db.models import Q, F
+from django.db.models.aggregates import Count, Min
 from store.models import Product, Customer, OrderItem, Order
 from django.http import HttpResponse
 
 
 def say_hello(request):
-    # select_related (1)
-    # prefetch_related (n)
-    # query_set = Product.objects.prefetch_related("promotions").all()
-    # query_set = Product.objects.select_related("collection").all()
-    # query_set = Product.objects.prefetch_related("promotions").select_related("collection").all()
-
-    # Get the last 5 orders with their customer and items includiing product
-    query_set = (
-        Order.objects.select_related("customer")
-        .prefetch_related("orderitem_set__product")
-        .order_by("-placed_at")[:5]
+    result = Product.objects.filter(id__lte=500).aggregate(
+        count_id=Count("id"), min_price=Min("unit_price")
     )
 
-    return render(request, "hello.html", {"name": "Ali", "products": list(query_set)})
+    return render(request, "hello.html", {"name": "Ali", "result": result})
+ 
