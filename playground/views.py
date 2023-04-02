0db493ed11cff09ba3ee9ad65bb6ac5e979be6c4
AliAkbarBadri <aabadri74@gmail.com>
2023-04-02 18:02:06 +0330

    add running raw sql example

--- playground/views.py
+++ playground/views.py
@@ -1,23 +1,19 @@
 from django.shortcuts import render
-from django.db import transaction
+from django.db import connection
 from store.models import Product, Customer, OrderItem, Order, Collection
 
 
 # decorate all say_hello function to run as a transaction
 @transaction.atomic()
 def say_hello(request):
-    order = Order.objects.create(customer_id=1)
-    item = OrderItem.objects.create(
-        order=order, product_id=1, quantity=1, unit_price=10
-    )
-    return render(request, "hello.html", {"name": "Ali"})
-
-# when we want run atomic part of function
-# def say_hello(request):
-#     # ....
-#     with transaction.atomic():
-#         order = Order.objects.create(customer_id=1)
-#         item = OrderItem.objects.create(
-#             order=order, product_id=1, quantity=1, unit_price=10
-#         )
+    # run raw query that the result fits in model
+    query_set = Product.objects.raw("select * from store_product")
+    query_set = Product.objects.raw("select ifd, title from store_product")
+    
+    # otherwise
+    with connection.cursor() as cursor:
+        cursor.execute("select * from store_product")
+        # run stored_procedure, the cleaner way
+        cursor.callproc("get_products", [1,2])
+        row = cursor.fetchone()
     return render(request, "hello.html", {"name": "Ali"})
