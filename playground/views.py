c4f00addd42c06868b21d65c8bf0143386002685
AliAkbarBadri <aabadri74@gmail.com>
2023-04-02 04:59:03 +0330

    Querying Generic Relationships

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count, ExpressionWrapper, DecimalField
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product, Customer, OrderItem, Order
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    query_set = Product.objects.annotate(
-        discounted_price=ExpressionWrapper(
-            F("unit_price") * 0.8, output_field=DecimalField()
-        )
+    # tags of a product id
+    content_type = ContentType.objects.get_for_model(Product)
+    query_set = TaggedItem.objects.select_related("tag").filter(
+        content_type=content_type,  # id of store product in django_content_type table
+        object_id=1,  # target product_id
     )
-    _ = list(query_set)
-    return render(request, "hello.html", {"name": "Ali"})
+    return render(request, "hello.html", {"name": "Ali", "tags": list(query_set)})
