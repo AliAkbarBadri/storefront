2e9c229ae849f40fec6253da5c8ce9d51e7ccd1c
AliAkbarBadri <aabadri74@gmail.com>
2023-04-02 05:06:12 +0330

    Custom Managers

--- playground/views.py
+++ playground/views.py
@@ -6,9 +6,5 @@ from tags.models import TaggedItem
 
 def say_hello(request):
     # tags of a product id
-    content_type = ContentType.objects.get_for_model(Product)
-    query_set = TaggedItem.objects.select_related("tag").filter(
-        content_type=content_type,  # id of store product in django_content_type table
-        object_id=1,  # target product_id
-    )
+    query_set = TaggedItem.objects.get_tags_for(Product, 1)
     return render(request, "hello.html", {"name": "Ali", "tags": list(query_set)})
