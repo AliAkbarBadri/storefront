bb6886a581d7e5545926be6473890370cc253ccb
AliAkbarBadri <aabadri74@gmail.com>
2023-04-05 05:41:04 +0330

    add some basic customization

--- store/admin.py
+++ store/admin.py
@@ -50,10 +50,16 @@ class InventoryFilter(admin.SimpleListFilter):
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    # if we want shoe __str__ of collection
-    # list_display = ["title", "unit_price", "inventory_status", "collection"]
-
-    # ow
+    # customize form
+    # only these fields in form of new product
+    # fields = ["title", "unit_price"]
+    # exclude these fields from form
+    # exclude = ["description"]
+    # readonly_fields = ["title"]
+    prepopulated_fields = {"slug": ["title"]}
+    
+    
+    
     actions = ["clear_inventory"]
     list_display = ["title", "unit_price", "inventory_status", "collection_title"]
     list_editable = ["unit_price"]
