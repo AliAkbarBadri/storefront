4c255907851df1cfc8941361e3c4d8a6f0759730
AliAkbarBadri <aabadri74@gmail.com>
2023-04-08 09:13:04 +0330

    create products url with djangorestframework

--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
+@api_view()
 def product_list(request):
-    return HttpResponse("ok")
+    return Response("ok")
