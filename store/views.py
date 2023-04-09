94da261060c2493ee8ffed5f5e22d913e53ae8a1
AliAkbarBadri <aabadri74@gmail.com>
2023-04-09 16:51:36 +0330

    get product in product_detail with try-except

--- store/views.py
+++ store/views.py
@@ -1,12 +1,22 @@
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view()
 def products_list(request):
     return Response("ok")
 
+
 @api_view()
 def product_detail(request, id):
-    return Response(id)
+    try:
+        product = Product.objects.get(pk=id)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        # return Response(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
