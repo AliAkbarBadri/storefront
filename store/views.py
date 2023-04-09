857f4c1bd0237ada50a376bd5d9ef207b4373a14
AliAkbarBadri <aabadri74@gmail.com>
2023-04-09 16:53:09 +0330

    get product in product_detail with shortcuts

--- store/views.py
+++ store/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
@@ -13,10 +14,6 @@ def products_list(request):
 
 @api_view()
 def product_detail(request, id):
-    try:
-        product = Product.objects.get(pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
-    except Product.DoesNotExist:
-        # return Response(status=404)
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
