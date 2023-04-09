fcfbc557ab30fc6a6ddad51e8fbb1181a9ffd3a8
AliAkbarBadri <aabadri74@gmail.com>
2023-04-09 17:02:18 +0330

    return list of all products in product_list

--- store/views.py
+++ store/views.py
@@ -2,14 +2,16 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_list_or_404, get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
 
 @api_view()
 def products_list(request):
-    return Response("ok")
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
