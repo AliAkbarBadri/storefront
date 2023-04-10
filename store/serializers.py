b49b2a9a12aa6db67514a456b3cf268debb583a0
AliAkbarBadri <aabadri74@gmail.com>
2023-04-10 04:46:22 +0330

    use model serializers to avoid redundancy

--- store/serializers.py
+++ store/serializers.py
@@ -1,19 +1,20 @@
 from decimal import Decimal
+from pyexpat import model
 from rest_framework import serializers
 from store.models import Collection, Product
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
 
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(
-        max_digits=6, decimal_places=2, source="unit_price"
-    )
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "unit_price", "price_with_tax", "collection"]
+
     price_with_tax = serializers.SerializerMethodField(method_name="calc_tax")
     collection = serializers.HyperlinkedRelatedField(
         queryset=Product.objects.all(), view_name="collection-detail"
