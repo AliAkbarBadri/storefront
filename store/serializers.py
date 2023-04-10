9970c225a7ae00d96d253305701716a44dc177d4
AliAkbarBadri <aabadri74@gmail.com>
2023-04-10 04:35:41 +0330

    serialize relationships with hyperlink

--- store/serializers.py
+++ store/serializers.py
@@ -15,7 +15,9 @@ class ProductSerializer(serializers.Serializer):
         max_digits=6, decimal_places=2, source="unit_price"
     )
     price_with_tax = serializers.SerializerMethodField(method_name="calc_tax")
-    collection = CollectionSerializer()
+    collection = serializers.HyperlinkedRelatedField(
+        queryset=Product.objects.all(), view_name="collection-detail"
+    )
 
     def calc_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -3,14 +3,14 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_list_or_404, get_object_or_404
-from .models import Product
-from .serializers import ProductSerializer
+from .models import Collection, Product
+from .serializers import CollectionSerializer, ProductSerializer
 
 
 @api_view()
 def products_list(request):
     queryset = Product.objects.select_related("collection").all()
-    serializer = ProductSerializer(queryset, many=True)
+    serializer = ProductSerializer(queryset, many=True, context={"request": request})
     return Response(serializer.data)
 
 
@@ -18,4 +18,11 @@ def products_list(request):
 def product_detail(request, id):
     product = get_object_or_404(Product, pk=id)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk=pk)
+    serializer = CollectionSerializer(collection)
     return Response(serializer.data)
