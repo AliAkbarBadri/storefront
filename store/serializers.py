e69e1da9c7e1f56dcc25b452a3359c8738834570
AliAkbarBadri <aabadri74@gmail.com>
2023-04-10 14:59:05 +0330

    add data validation

--- store/serializers.py
+++ store/serializers.py
@@ -16,9 +16,15 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = ["id", "title", "unit_price", "price_with_tax", "collection"]
 
     price_with_tax = serializers.SerializerMethodField(method_name="calc_tax")
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Product.objects.all(), view_name="collection-detail"
-    )
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Product.objects.all(), view_name="collection-detail"
+    # )
 
     def calc_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+    # this is just a example and is not real
+    # def validate(self, data):
+    #     if data["password"] != data["confirm_password"]:
+    #         return serializers.ValidationError("Passwords do not match!")
+    #     return data
--- store/views.py
+++ store/views.py
@@ -1,17 +1,32 @@
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.request import Request
 from rest_framework import status
 from django.shortcuts import get_list_or_404, get_object_or_404
 from .models import Collection, Product
 from .serializers import CollectionSerializer, ProductSerializer
 
 
-@api_view()
+@api_view(["GET", "POST"])
 def products_list(request):
-    queryset = Product.objects.select_related("collection").all()
-    serializer = ProductSerializer(queryset, many=True, context={"request": request})
-    return Response(serializer.data)
+    if request.method == "GET":
+        queryset = Product.objects.select_related("collection").all()
+        serializer = ProductSerializer(queryset, many=True, context={"request": request})
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ProductSerializer(data=request.data)
+        # method1
+        # if serializer.is_valid():
+        #     serializer.validated_data
+        #     return Response("ok")
+        # else:
+        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        # method2: cleaner
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
+        return Response("ok")
 
 
 @api_view()
