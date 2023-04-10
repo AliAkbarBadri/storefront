67570ced3f6c4b3fbd8b6b8287e0a4433a94e6ee
AliAkbarBadri <aabadri74@gmail.com>
2023-04-10 04:28:51 +0330

    serialize relationships with nested object

--- store/serializers.py
+++ store/serializers.py
@@ -3,6 +3,11 @@ from rest_framework import serializers
 from store.models import Collection, Product
 
 
+class CollectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
+
+
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
@@ -10,7 +15,7 @@ class ProductSerializer(serializers.Serializer):
         max_digits=6, decimal_places=2, source="unit_price"
     )
     price_with_tax = serializers.SerializerMethodField(method_name="calc_tax")
-    collection = serializers.StringRelatedField()
+    collection = CollectionSerializer()
 
     def calc_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
