8483026ffbf29680265ac017a428d25417013d9d
AliAkbarBadri <aabadri74@gmail.com>
2023-04-28 23:41:14 +0330

    make product views class-based

--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.products_list),
-    path("products/<int:id>/", views.product_detail),
+    path("products/", views.ProductsList.as_view()),
+    path("products/<int:id>/", views.ProductDetail.as_view()),
     path("collections/", views.collections_list),
     path("collections/<int:pk>/", views.collection_detail, name="collection-detail"),
 ]
