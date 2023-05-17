ff8e4ce1fbc2a712643876afa7f9059219dd38e7
AliAkbarBadri <aliakbarbadr@gmail.com>
2023-05-17 08:30:26 +0330

    add DefaultRouter

--- store/urls.py
+++ store/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from django.urls.conf import include
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from . import views
 
-router = SimpleRouter()
+"""
+DefaultRouter: 
+    - has api root page (http://127.0.0.1:8000/store/) 
+    - returns json for any path like http://127.0.0.1:8000/store/products/1.json 
+"""
+router = DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
 
