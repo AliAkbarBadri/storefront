bb2df53f447bd9f6166c97e72f632e6f9643b88b
AliAkbarBadri <aliakbarbadr@gmail.com>
2023-05-31 08:21:50 +0330

    override destroy method instead of delete in CollectionViewSet

--- store/views.py
+++ store/views.py
@@ -26,17 +26,12 @@ class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count("products")).all()
     serializer_class = CollectionSerializer
 
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs["pk"]).count() > 0:
             return Response(
                 {
                     "error": "Collection Cannot be deleted, there is products associated!"
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    def destroy(self, request, *args, **kwargs):
         return super().destroy(request, *args, **kwargs)
